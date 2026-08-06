2d73324006b309f16dd6d1567b66bf05accb66ec
Quu <ngphuquy241@gmail.com>
2026-08-06 09:40:37 +0700

    feat(auth): init authentication

--- quan-ly-nha-hang-APIs/src/main/java/com/npq/quanlynhahangapis/repository/NguoiDungRepository.java
+++ quan-ly-nha-hang-APIs/src/main/java/com/npq/quanlynhahangapis/repository/NguoiDungRepository.java
@@ -1,21 +1,13 @@
 package com.npq.quanlynhahangapis.repository;
 
 import com.npq.quanlynhahangapis.entity.NguoiDung;
-import org.jspecify.annotations.NonNull;
 import org.jspecify.annotations.NullMarked;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 import java.util.Optional;
 
 @NullMarked
 public interface NguoiDungRepository extends JpaRepository<NguoiDung, Integer> {
-    @Override
-    List<NguoiDung> findAll();
-//    Optional<NguoiDung> taoNguoiDung();
-//    @Override
-//    Optional<NguoiDung> findById(Integer integer);
-//
-
+    Optional<NguoiDung> findByTaiKhoan(String taiKhoan);
 
 }
