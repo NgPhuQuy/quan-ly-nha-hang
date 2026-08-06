c55cb0512520371c85fda32d17efeb58ed20e5f0
Quu <ngphuquy241@gmail.com>
2026-08-06 23:20:32 +0700

    feat(JWT): init JWT 10%

--- quan-ly-nha-hang-APIs/src/main/java/com/npq/quanlynhahangapis/dto/request/NguoiDungRequest.java
+++ quan-ly-nha-hang-APIs/src/main/java/com/npq/quanlynhahangapis/dto/request/NguoiDungRequest.java
@@ -2,12 +2,13 @@ package com.npq.quanlynhahangapis.dto.request;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 public record NguoiDungRequest(
         @NotBlank(message = "Tài khoản không được để trống!")
         String taiKhoan,
-        @NotBlank
+        @NotBlank(message = "Mật khẩu không được để trống!")
         @Size(min = 8, message = "Mật khẩu tối thiểu 8 ký tự!")
         String matKhau,
         String avatar,
@@ -15,6 +16,8 @@ public record NguoiDungRequest(
         String ten,
         @Email(message = "Email không hợp lệ!")
         String email,
+        @Pattern(regexp = "^0\\d{9}$", message = "Số điện thoại không hợp lệ!")
+        @NotBlank(message = "Số điện thoại không được để trống!")
         String soDienThoai
 ) {
 }
