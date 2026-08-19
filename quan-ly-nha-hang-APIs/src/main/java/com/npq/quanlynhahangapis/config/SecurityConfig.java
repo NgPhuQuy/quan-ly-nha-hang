5d0c6d68170b1df669778100c4c246416e779538
Quu <ngphuquy241@gmail.com>
2026-08-19 19:52:09 +0700

    feat(FE): 5%

--- quan-ly-nha-hang-APIs/src/main/java/com/npq/quanlynhahangapis/config/SecurityConfig.java
+++ quan-ly-nha-hang-APIs/src/main/java/com/npq/quanlynhahangapis/config/SecurityConfig.java
@@ -44,14 +44,14 @@ public class SecurityConfig {
     }
 
     @Bean
-    public CorsConfigurationSource corsConfigurationSource(){
+    public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
         corsConfiguration.setAllowedOrigins(List.of("http://localhost:5173"));
         corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         corsConfiguration.setAllowedHeaders(List.of("*"));
         corsConfiguration.setAllowCredentials(true);
 
-        UrlBasedCorsConfigurationSource source =new UrlBasedCorsConfigurationSource();
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", corsConfiguration);
         return source;
     }
